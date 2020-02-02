87ca76e87b5f4f842fc6954b94c71694185094b5
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-02 07:45:23 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy[:Montague][hero][]
+epic_tragedy[:Montague][:hero][:status]
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
